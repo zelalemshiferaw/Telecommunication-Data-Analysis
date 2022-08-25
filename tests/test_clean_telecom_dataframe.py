cc1ff4c7c362487ac05b780ff202256599435951
zelalem shiferaw <zelalemshiferaw71921@gmail.com>
2022-08-25 13:21:51 +0300

    Update unitest

--- tests/test_clean_telecom_dataframe.py
+++ tests/test_clean_telecom_dataframe.py
@@ -13,7 +13,7 @@ class TestCleaTelco(unittest.TestCase):
     
     def setUp(self):
         self.clean_telecom = clean_telecom_dataframe.Telecom()
-    def test_convert_to_string(self):
+    def ctest_convert_to_string(self):
         self.assertEqual(self.clean_telecom.convert_to_string(df, 'Handset Type')['Handset Type'].dtype, 'string')
 
     def test_convert_to_datetime(self):
